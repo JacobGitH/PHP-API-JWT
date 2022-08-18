508f38073a436dbc1a953e7b59dc8ab5610c1c8d
Jakub Kopecky <kopecek121@gmail.com>
2022-08-18 10:25:57 +0200

    commit after my github fuck up

---
+++ paths.php
@@ -0,0 +1,10 @@
+<?php
+//to small project to create autoloader
+declare(strict_types=1);
+require_once('../../vendor/autoload.php');
+require_once('../../auth/auth_dat.php');
+require_once('../../auth/authenticate.php');
+include_once '../../config/Database.php';
+include_once '../../models/Post.php';
+include_once '../../models/User.php';
+
